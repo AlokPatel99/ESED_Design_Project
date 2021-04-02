f66a1658bc4a7455d8c0de016d9c2b75b0f698f2
Alok Patel <49090662+AlokPatel99@users.noreply.github.com>
2021-04-02 09:37:30 -0400

    Update Data_Gen.m

--- Data_Gen.m
+++ Data_Gen.m
@@ -6,7 +6,7 @@
 
 %constant values
 fill_factor = 0.5; %fill facotr of core
-eta_free = 4*pi*1e-7; %free space permeability 
+uo = 4*pi*1e-7; %free space permeability
 %eta_r = 1000; %relative permeablilty; depends on the type of core materials
 i = 2.5; %constnat current
 
