425991c19497f234b9ea458d5711fa7759c703a5
Alok Patel <49090662+AlokPatel99@users.noreply.github.com>
2021-04-02 08:56:09 -0400

    Add files via upload

---
+++ Data_Gen.m
@@ -0,0 +1,10 @@
+%% ------------ ECSE-549: ESED - Project ---------- %%
+% This file is used to create the data set for the training of the NN.
+
+
+%% --- Functions --- %%
+
+
+
+
+%% ----- Code END ----- %%%
