de4f6e6ee917ac3f7baeccb19e5e9977adfea1d7
Alok Patel <49090662+AlokPatel99@users.noreply.github.com>
2021-04-02 09:35:16 -0400

    Delete volume.m

--- volume.m
+++
@@ -1,6 +0,0 @@
-function v = volume(h,d,A_core,w,tw,tc,tg,lg)
-%VOLUME Summary of this function goes here
-%   Detailed explanation goes here
-    v = h*d*A_core + 2*(w-tw)*tc*A_core + (h-2*tc - lg)*tg*A_core;
-end
-
