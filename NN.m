21ee08f04c6594c07b751938992c8aab97d80583
Alok Patel <49090662+AlokPatel99@users.noreply.github.com>
2021-04-08 19:22:17 -0400

    Update NN.m
    Just notes are added, please look at it.

--- NN.m
+++ NN.m
@@ -1,6 +1,11 @@
 %% ------------ ECSE-549: ESED - Project ---------- %%
 % This file is used to train and test the NN.
 
+%Note: 
+%1) For the crosssectional area of the copper, we will check the area
+%   and accordingly set the guage and store as o/p.
+%2) For input, if the the space is greater than 30.25 cm^2, then can take
+%   the random from range or the max one. 
 
 %%
 input_to_NN = (readtable('CCoreInputData.csv'))';
