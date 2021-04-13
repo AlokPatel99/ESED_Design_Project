13454e1a133520c78c3bc34aaf82559fbdc38940
Alok Patel <49090662+AlokPatel99@users.noreply.github.com>
2021-04-13 16:41:31 -0400

    Update Data_Gen.m

--- Data_Gen.m
+++ Data_Gen.m
@@ -1,8 +1,6 @@
 %% ------------ ECSE-549: ESED - Project ---------- %%
 % This file is used to create the data set for the training of the NN.
 
-
-
 %% --- Creating Inputs for the equations and storing data --- %%
 
 %Number of examples are set at N_ex
@@ -11,37 +9,25 @@ N_ex = 5000;
 %constant values
 fill_factor = 0.5; %fill facotr of core
 uo = 4*pi*1e-7; %free space permeability 
-%eta_r = 1000; %relative permeablilty; depends on the type of core materials
-%i = 2.5; %constnat current
-%{
-%Random generation of Inductance Value
-lmin = 100e-6;
-lmax = 100e-3;
-Lip = (lmin - lmax)*rand(1,N_ex) + lmax;
-%}
+
 %Random generation of Turns
 Nmin = 5;
-Nmax = 60;
+Nmax = 100;
 Nop = randi([Nmin, Nmax],1,N_ex);
-%{
-%Random generation of depth of the core
-dmin = 0.5;
-dmax = 1.5;
-dop = (dmin - dmax) + rand(1,N_ex) + dmax;
-%}
-%Random generation of height of the core
+
+%Random generation of height of the core(m)
 hmin = 2e-2;
 hmax = 5e-2;
-hop = (hmin - hmax) + rand(1,N_ex) + hmax;
+hop = (hmin - hmax)*rand(1,N_ex) + hmax;
 
-%Random generation of widht of the core
+%Random generation of widht of the core(m)
 wmin = 2e-2;
 wmax = 5e-2;
 wop = (wmin - wmax)*rand(1,N_ex)  + wmax;
 
-%Random generation of cross-sectional area of the core
-Acmin = 0.5e-4;
-Acmax = 1e-4;
+%Random generation of cross-sectional area of the core(m^2)
+Acmin = 0.25e-4;
+Acmax = 0.6e-4;
 Acip = (Acmin - Acmax)*rand(1,N_ex)  + Acmax;
 
 %Setting up other cross sectional area for the core
@@ -52,8 +38,8 @@ A_air = Acip;
 
 %Setting up the width of core or the thickness of core from
 %all direction as it is similar.
-d = sqrt(Acip);
-tw = d;
+dop = sqrt(Acip);
+tw = dop;
 tc = tw;
 tg = tc;
 
@@ -64,12 +50,12 @@ mat_core = [1,2];
 material = ["Silicon Steel","Ferrites"];
 ur = [1000,500];
 
-%Random generation of area to fit Inductor
+%Random generation of area to fit Inductor(m^2)
 Amin = 4e-4;
 Amax = 30.25e-4;
 Aip = (Amin - Amax)*rand(1,N_ex)  + Amax;
 
-%Random generation of the air gap length
+%Random generation of the air gap length(m)
 lgmin = 1e-3;
 lgmax = 5e-3;
 lgop = (lgmin - lgmax)*rand(1,N_ex) + lgmax;
@@ -100,6 +86,7 @@ v1 = zeros(1,N_ex);
 v2 = zeros(1,N_ex);
 cost_op = zeros(1,N_ex);
 mat_ip = strings(0);
+mat_no_ip = zeros(1,N_ex);
 
 % Data set for the guage cable type
 for i=1:N_ex
@@ -134,11 +121,11 @@ while i<N_ex
             TR(i+1) = reluctance(hop(i+1),tc(i+1),ur(m),uo,A_back(i+1),A_top(i+1),A_pole(i+1),tw(i+1),wop(i+1),tg(i+1),A_air(i+1),lgop(i+1));
             L(i+1) = inductance(Nop(i+1),TR(i+1));
             A_space(i+1) = hop(i+1)*wop(i+1);
-            %A_wire(i+1) = wire_func(Wa(i+1),fill_factor,Nop(i+1));
             v1(i+1) = volume_core(hop(i+1),dop(i+1),Acip(i+1),wop(i+1),tw(i+1),tc(i+1),tg(i+1),lgop(i+1));
             v2(i+1) = volume_coil(dw(i+1),tw(i+1),Acwire_op(i+1),Nop(i+1));
             cost_op(i+1) = cost_total(v1(i+1),v2(i+1),mat_core(m));
             mat_ip(i+1) = material(m); %material string vector
+            mat_no_ip(i+1) = mat_core(m);
             i = i+1;
         end
     end
@@ -149,10 +136,11 @@ end
 % Data variables are defined for storing the data
 
 %Inputs for the NN
-L_ip_d = L';            %Inductance
+L_ip_d = L';                %Inductance
 mat_ip_d = mat_ip';          %Material
-A_core_ip_d = Acip';       %Area of the core crosssectional area
+A_core_ip_d = Acip';        %Area of the core crosssectional area
 A_space_ip_d = A_space';      %Area of the space available to place the inductor
+Imax_ip_d = Imax';          %Maximum current capability of the core
 
 %Output for the NN
 cost_op_d = cost_op';            %Total cost for manufacturing the inductor 
@@ -162,14 +150,13 @@ d_op_d = dop';            %depth of the core
 w_op_d = wop';            %width of the core
 h_op_d = hop';            %height of the core
 A_w_cross_d = Acwire_op';       %Crosssectional area of the wire winding
-Imax_ip_d = Imax';          %Maximum current capability of the core
 l_g_d = lgop';             %Lenght of the airgap
 
 %inputs write to csv
 
-input_data = [L_ip_d, mat_ip_d, A_core_ip_d, A_space_ip_d,Imax_ip_d]; %matrix input data
+input_data = [L_ip_d, mat_ip_d, A_core_ip_d, A_space_ip_d, Imax_ip_d]; %matrix input data
 T = array2table(input_data);
-T.Properties.VariableNames(1:5) = {'Inductance','Material','Cross Sectional Area of Core','Area of the space','Max Current Capability'};
+T.Properties.VariableNames(1:5) = {'Inductance','Material','Cross Sectional Area of Core','Area of the space', 'Max Current capability'};
 %T.Properties.VariableNames(1:4) = {'Inductance','Material','Cross Sectional Area of Core','Area of the space'};
 writetable(T,'CCoreInputData.csv')
 
@@ -181,26 +168,15 @@ S.Properties.VariableNames(1:8) = {'Inductance','Air Gap Length','Height','Width
 %S.Properties.VariableNames(1:4) = {'Air Gap Length','Height','Width','Number of Turns','Cost','Wire Cross Sectional Area'};
 writetable(S,'CCoreOutputData.csv')
 
+
 %% --- Functions for equations --- %%
-%{
-function f = flux(mmf,tr)
-%FLUX Summary of this function goes here
-%   Detailed explanation goes here
-    f = mmf/tr;
-end
-%}
+
 function I = inductance(n,tr)
 %INDUCTANCE Summary of this function goes here
 %   Detailed explanation goes here
         I = (n^2)/tr;
 end
-%{
-function m = mmf(i,n)
-%MMF Summary of this function goes here
-%   Detailed explanation goes here
-    m = i*n/2;
-end
-%}
+
 function R = reluctance(h,tc,ur,uo,A_back,A_top,A_pole,tw,w,tg,A_air,lg)
 %RELUCTANCE Summary of this function goes here
 %   Detailed explanation goes here
@@ -218,13 +194,7 @@ function v1 = volume_core(h,d,A_core,w,tw,tc,tg,lg)
 %   Detailed explanation goes here
     v1 = h*d*A_core + 2*(w - tw)*tc*A_core + (h - 2*tc - lg)*tg*A_core;
 end
-%{
-function w = wire_func(wa,ku,n)
-%WIRE_FUNC Summary of this function goes here
-%   Detailed explanation goes here
-    w = wa*ku/n;
-end
-%}
+
 function v2 = volume_coil(dw,tw,Awind,N)
 %VOLUME SUMMARY of this function goes here
 %   Detailed explaination goes here
@@ -243,4 +213,4 @@ c3 = 0.1*(c1+c2);                   %fixed cost of 10% of overall is considered
 c = c1 + c2 + c3; 
 end
 
-%% ----- Code END ----- %%%
+%% ----- Code END ----- %%
